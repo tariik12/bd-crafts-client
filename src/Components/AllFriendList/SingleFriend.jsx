4fdf321d155cb1da5e28c5ada761762b5be50df0
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-18 12:16:53 +0600

    all friend list link done

--- src/Components/AllFriendList/SingleFriend.jsx
+++ src/Components/AllFriendList/SingleFriend.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const SingleFriend = ({ friend }) => {
   const { profilePicture, fullName } = friend;
   return (
     <div className="flex gap-2 items-center py-2">
       <img src={profilePicture} alt="" className="w-10 h-10 rounded-full " />
-      <p className="text-xl font-semibold">{fullName}</p>
+      <Link
+        to="/"
+        className="text-xl font-semibold hover:underline hover:text-blue-500"
+      >
+        {fullName}
+      </Link>
     </div>
   );
 };
