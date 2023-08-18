7eef974293c916c2b83f8a143377ce7b937142a6
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-18 10:24:08 +0600

    data loaded

---
+++ src/Components/AboutCard/AboutCard.jsx
@@ -0,0 +1,12 @@
+const AboutCard = () => {
+  return (
+    <>
+      <section className="bg-slate-300 shadow-lg rounded-xl ">
+        <p>About</p>
+        <p></p>
+      </section>
+    </>
+  );
+};
+
+export default AboutCard;
