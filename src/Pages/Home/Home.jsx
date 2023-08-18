7403e765cd43546a70a2f882216ac4c0f3650728
Rayhan Shorker <raihanshorker56@gmail.com>
2023-08-18 11:38:48 +0600

    fixed bug

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,33 @@
-import CreateStory from "../../Components/CreateStory/CreateStory";
+import { useEffect, useState } from "react";
 import DisplayPost from "../../Components/DisplayPost/DisplayPost";
 import CreatePost from "../../Components/Navber/CreatePosts/CreatePost";
 import LeftSideBar from "./LeftSideBar/LeftSideBar";
 import RightSideBar from "./RightSideBar/RightSideBar";
+import CreateStory from "../../Components/CreateStory/CreateStory";
 
 
 const Home = () => {
+  const [posts,setPosts]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:5000/allposts")
+    .then((res)=>res.json())
+    .then((data)=>{
+      setPosts(data)
+    })
+  },[])
+  console.log(posts);
     return (
         <div className="grid grid-flow-col">
         <div className="grid-cols-1 bg-red-600">
         <LeftSideBar/>
         </div>
-        <div className="grid-cols-10 bg-orange-500">
-        
-          <CreateStory/>
+        <div className="grid-cols-10 ">
+        <CreateStory/>
           <CreatePost/>
-          <DisplayPost/>
+          {
+            posts?.map((post)=><DisplayPost key={post?._id} post={post}/>)
+          }
+          
         </div>
         <div className="grid-cols-1 bg-green-500">
         <RightSideBar/>
