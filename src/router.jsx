37dfc426f418d396613bfe39fb88c88f3785ebe4
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-25 11:41:35 +0600

    login page route change

--- src/router.jsx
+++ src/router.jsx
@@ -1,54 +1,47 @@
-import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
-  import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "./Layout/Main";
+import Login from "./Pages/Authentication/Login";
+import Register from "./Pages/Authentication/Register";
+import FindFriend from "./Pages/FindFriend/FindFriend";
 import Home from "./Pages/Home/Home";
 import InboxMessages from "./Pages/InboxMessages/InboxMessages";
 import Profile from "./Pages/Profile/Profile";
-import FindFriend from "./Pages/FindFriend/FindFriend";
-import Login from "./Pages/Authentication/Login";
-import Register from "./Pages/Authentication/Register";
+import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      //ahaduzzaman ahad vai
+      {
+        path: "/inboxMessages",
+        element: <InboxMessages />,
+      },
+      //Mahdi vai
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      //Rabeya Akter
+      {
+        path: "/findFriend",
+        element: <FindFriend />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main/>,
-      children:[
-        {
-            path:'/',
-            element:<Home/>
-        },
-        //ahaduzzaman ahad vai
-        {
-          path:'/inboxMessages',
-          element:<InboxMessages/>
-        },
-        //Mahdi vai
-        {
-          path:'/profile',
-          element:<Profile/>
-        },
-        //Rabeya Akter
-        {
-          path:'/findFriend',
-          element:<FindFriend/>
-        },
-        {
-          path:'/login',
-          element:<Login/>
-        },
-        {
-          path:'/register',
-          element:<Register/>
-        }
-        
-
-      ]
-    },
-  ]);
-
-  export default router;
-  
+export default router;
