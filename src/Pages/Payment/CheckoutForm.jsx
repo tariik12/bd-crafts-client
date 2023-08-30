67b232fa2411d1801934f9588892adf97af812d9
tarikul-islam <tariikul12@gmail.com>
2023-08-30 08:28:49 +0600

    payment working

--- src/Pages/Payment/CheckoutForm.jsx
+++ src/Pages/Payment/CheckoutForm.jsx
@@ -1,8 +1,10 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState()
     const handleSubmit =async(event) =>{
         event.preventDefault();
 if(!stripe || !elements){
@@ -14,8 +16,21 @@ if(card == null){
     return
 }
 console.log("card", card)
+const {error, paymentMethod} = await stripe.createPaymentMethod({
+    type:"Card",
+    card
+})
+if(error){
+    console.log('Error', error)
+    setCardError(error.message)
+}
+else{
+    setCardError('')
+    console.log('payment method',paymentMethod)
+}
     }
     return (
+        <>
         <form className="w-1/2 mx-auto mt-10" onSubmit={handleSubmit}>
         <CardElement
           options={{
@@ -37,6 +52,8 @@ console.log("card", card)
           Pay
         </button>
       </form>
+      {cardError && <p className="text-red-600">{cardError}</p>}
+        </>
     );
 };
 
