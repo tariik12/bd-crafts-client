cc835854f887e48f1f6cb30bdbb7277067bf3296
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-08-31 14:31:15 +0600

    Emoji send succesfully

--- src/Components/ChatBoxTyping/ChatBoxTyping.jsx
+++ src/Components/ChatBoxTyping/ChatBoxTyping.jsx
@@ -1,14 +1,21 @@
 import React, { useContext, useRef, useState } from "react";
-import { FaImage, FaIcons } from "react-icons/fa";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
-import { db, storage } from "../../../firebase.config"; 
+import { db, storage } from "../../../firebase.config";
+import EmojiPicker from 'emoji-picker-react';
+import { FaIcons, FaImage } from 'react-icons/fa';
+
+const style = {
+  inputCls: `block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`,
+  textEmojiBtn: `inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 bg-gray-200`,
+};
 
 const ChatBoxTyping = () => {
   const { user } = useContext(AuthContext);
   const [input, setInput] = useState("");
   const [imageURL, setImageURL] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const fileInputRef = useRef(null);
 
   const sendMassage = async (e) => {
@@ -26,7 +33,7 @@ const ChatBoxTyping = () => {
       uid: uid,
       timestamp: serverTimestamp(),
       photoURL: photoURL,
-      image: imageURL, 
+      image: imageURL,
     });
     setInput("");
     setImageURL("");
@@ -57,21 +64,25 @@ const ChatBoxTyping = () => {
       const storageRef = ref(storage, `images/${file.name}`);
       await uploadString(storageRef, imageBase64, "data_url");
       const downloadURL = await getDownloadURL(storageRef);
-      setImageURL(downloadURL); 
-      setInput(""); 
+      setImageURL(downloadURL);
+      setInput("");
     };
   };
 
+  const handleEmojiSelect = (emoji) => {
+    const emojiText = emoji.native || emoji.emoji; 
+    setInput(input + emojiText);
+  };
   return (
     <div className="w-full">
-      <form onSubmit={sendMassage}>
-        <div className="flex items-center px-3 py-2 rounded-lg ">
+      <form  >
+        <div className="flex items-center px-3 py-2 rounded-lg gap-2">
           <button
             onClick={openImageFile}
             type="button"
-            className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            className={style.textEmojiBtn}
           >
-            <FaImage size={30}></FaImage>
+            <FaImage size={25}></FaImage>
             <input
               ref={fileInputRef}
               type="file"
@@ -81,15 +92,32 @@ const ChatBoxTyping = () => {
             />
             <span className="sr-only">Upload image</span>
           </button>
+          <button
+            className={style.textEmojiBtn}  type="button"
+            onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+            
+          >
+            <FaIcons size={25}></FaIcons>
+          </button>
+
+          {showEmojiPicker && (
+            <div className="absolute z-10 bottom-14 right-0">
+              <EmojiPicker 
+              onEmojiClick={handleEmojiSelect}
+              />
+             
+            </div>
+          )}
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
             id="chat"
             rows="1"
-            className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className={style.inputCls}
             placeholder="Your message..."
           ></input>
-          <button className="btn bg-sky-950" type="submit">
+          <button className="btn bg-sky-700 text-white" type="submit"
+          onClick={sendMassage}          >
             Send
           </button>
         </div>
--- src/Components/Messeges/Messages/LeftSide/LeftSide.jsx
+++ src/Components/Messeges/Messages/LeftSide/LeftSide.jsx
@@ -14,7 +14,7 @@ const LeftSide = () => {
             .then(data => setFriends(data))
     }, [])
     const onlineFriend = friends?.filter(friend => friend?.status === "online");
-    console.log(onlineFriend);
+    // console.log(onlineFriend);
     return (
         <div className="flex">
             <div className="w-16 border-r-2 border-black">
