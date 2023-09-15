b763516a0d0994332c5d73dd141e865d3b8afc6b
tarikul-islam <tariikul12@gmail.com>
2023-09-15 12:42:44 +0600

    sume update

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -6,11 +6,11 @@ const Shop = () => {
   return (
     <>
       <Container>
-        <section className="grid grid-cols-1 md:grid-cols-12">
+        <section className="grid grid-cols-1 md:grid-cols-12 ">
           <div className="md:col-span-2 mt-7 ">
             <Categories></Categories>
           </div>
-          <div className="md:col-span-10">
+          <div className="md:col-span-10 ">
             <Outlet></Outlet>
           </div>
         </section>
