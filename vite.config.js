0f5a895b3b2f070e284458e8712e88db8830bd38
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-09-20 01:16:35 +0600

    vite page error done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import commonjs from 'vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [commonjs({
     filter(id) {
       if (id.includes('node_modules/react-chat-engine')) {
@@ -12,8 +11,4 @@ export default defineConfig({
       }
     },
   }),react()],
-=======
-  plugins: [react(),],
-  
->>>>>>> b7a4033893c57b9c589de436213b72cf5b52db0a
 })
